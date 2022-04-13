328a01c7c59e18ea95f44ae54a0374d4c231c6aa
Dulaksha <dulakshakavi@gmail.com>
2022-04-13 20:04:38 +0530

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route}
 from 'react-router-dom'
-import {ToastContainer } from 'react-toastify' 
+import {ToastContainer} from 'react-toastify' 
 import 'react-toastify/dist/ReactToastify.css'
 
 import Explore from "./pages/Explore";
